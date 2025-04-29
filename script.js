bf69e1ace56a78db3e328159482d452743d0d98a
mugabi praise <mugabipraise4@gmail.com>
2025-04-29 05:32:39 +0000

    practiced module 1 & 2 from chatgpt course. ie introduction to javascript and datatypes and operator. i find string literals good

---
+++ script.js
@@ -0,0 +1,57 @@
+
+//exercise 1.
+// Create three variables:
+
+// Your full name (string).
+
+// Your age (number).
+
+// Whether you are a student (boolean).
+
+let myFullName = 'mugabi praise'; //string
+let myAge = 34; //number
+let amStudent = true //boolean
+
+//exercise 2.
+//what are the datatypes of the following? write them in a comment.
+const a = "5"; //string
+const b = 5; //number
+const c = true;//boolean
+const d = null; // null
+const e = undefined; //undefined
+const f = {name: "Alice", age: 22}; //object
+
+//exercise three.
+// Solve these:
+
+10 % 3 //1
+
+2 ** 4 // 16
+
+5 === "5" //false
+
+5 == "5" //true
+
+true && false //false
+
+//exercise four
+let text = "JavaScript Programming";
+
+// Answer the following:
+
+//     How many characters are in text?
+
+//     Change all letters in text to uppercase.
+
+//     Check if text includes the word "Program".
+
+//     Extract only the word "Script" from text using slice.
+
+charactersInText = 22;
+console.log(text.toUpperCase());
+console.log(text.includes('Program'));
+console.log(text.slice(4,10));
+
+
+console.log(`hi there, my name is ${myFullName} and i am ${myAge} years old`);
+
